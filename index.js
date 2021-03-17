a69d402faf929edc3e4500c7665bad3a4f662390
MaxWasTaked <61461142+Yonaga678@users.noreply.github.com>
2021-03-18 00:58:41 +0200

    Update index.js

--- index.js
+++ index.js
@@ -2,12 +2,15 @@ const Discord = require('discord.js');
 const bot = new Discord.Client();
 const {get} = require("snekfetch");
 const PREFIX = "-";
-const fs = require('fs');
 
+
+const fs = require('fs');
+bot.commands = new Discord.Collection();
+ 
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
 for(const file of commandFiles){
     const command = require(`./commands/${file}`);
-
+ 
     bot.commands.set(command.name, command);
 }
 
