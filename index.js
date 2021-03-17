c73cc84c75a16845c3c25a376b90bd7f891bd666
MaxWasTaked <61461142+Yonaga678@users.noreply.github.com>
2021-03-17 23:25:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -108,8 +108,23 @@ bot.on('message', (message) => {
 
 
 bot.on('message', (message) => {
-    if(message.content('-kick')) {
-        message.author('kick method is running' + args);
+    if(!message.guild) return;
+    if(message.content.startsWith('!kick')) {
+        const user = message.mentions.users.first();
+        if(user) {
+            const member = message.guild.member(user);
+            if(member) {
+                member
+                    .kick('Optional reason that will display in the audit logs')
+                    .then(() => {
+                    message.reply(`Successfully kicked ${user.tag}!`);
+                });
+            } else {
+                message.reply("That user isn't in this guild!");
+            }
+        } else {
+            message.reply("You didn't mention the user to kick!");
+        }
     }
 });
 
