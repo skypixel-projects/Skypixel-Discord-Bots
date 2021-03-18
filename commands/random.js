2846b5976f2b82f0b0b1ebffc39da5aba0576cbf
MaxWasTaked <61461142+Yonaga678@users.noreply.github.com>
2021-03-18 11:33:04 +0200

    Update random.js

--- commands/random.js
+++ commands/random.js
@@ -3,14 +3,10 @@ module.exports = {
     description: "This command is for helping the members",
     execute(message, args){
         if(message.guild.id == '672018546125045760') {
-            try {
-                get('https://aws.random.cat/meow').then(response => {
-                    message.channel.send({files: [{attachment: response.body.file, name: `cat.${response.body.file.split('.')[4]}`}]});
-                    console.log('random cat picture');
-                })
-            } catch (e) {
-                console.log('error!');
-            }
+            get('https://aws.random.cat/meow').then(response => {
+                message.channel.send({files: [{attachment: response.body.file, name: `cat.${response.body.file.split('.')[4]}`}]});
+                console.log('random cat picture');
+            });
         }
     }
 }
