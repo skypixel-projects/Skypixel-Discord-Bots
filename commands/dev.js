4df8944798edf581db4fe118d88e1a3acb8ad00c
MaxWasTaked <61461142+Yonaga678@users.noreply.github.com>
2021-03-18 23:23:04 +0200

    Update dev.js

--- commands/dev.js
+++ commands/dev.js
@@ -1,13 +1,13 @@
 const Discord = require('discord.js'); 
 const fs = require('fs');
-const readline = require('readline');
 
 module.exports = {
     name: "dev",
     description: "",
     execute(message, args){
         const path = "./language/lang_en.yml";
-        const content = fs.readFileSync(path, 'utf-8');
-        message.channel.send(content, 2);
+        const content = fs.readFileSync(path, 'utf-8', line(2));
+        if(fs.readFile)
+        message.channel.send(content);
     }
 }
