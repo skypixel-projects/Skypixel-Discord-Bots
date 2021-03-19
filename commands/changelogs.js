c6144c336832d7a94426071f3f6a9fefd7dd1217
MaxWasTaked <61461142+Yonaga678@users.noreply.github.com>
2021-03-19 10:24:52 +0200

    19.03.2021 10:24

--- commands/changelogs.js
+++ commands/changelogs.js
@@ -1,12 +1,13 @@
+const Discord = require('discord.js'); 
+
 module.exports = {
     name: "changelogs",
     description: "All the new changelogs!",
     execute(message, args){
         if(message.guild.id == '672018546125045760') {
             message.delete();
-            message.channel.send('*Here you can find the latest updates!*');
-            message.channel.send('https://github.com/Yonaga678/DEVDiscord');
-
+            //message.channel.send('*Here you can find the latest updates!*');
+            //message.channel.send('https://github.com/Yonaga678/DEVDiscord');
             const changelogsEmbed = new Discord.MessageEmbed()
                 .setColor('#038cfc')
                 //.setTitle('**LIST OF ALL COMMAND FOR PURO BOT**')
