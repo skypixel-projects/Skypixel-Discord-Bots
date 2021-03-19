de3aaa17c2cc2b2f25f77f1f948dbc06da6d31b5
MaxWasTaked <61461142+Yonaga678@users.noreply.github.com>
2021-03-19 11:29:41 +0200

    Update command_handler.js

--- handlers/command_handler.js
+++ handlers/command_handler.js
@@ -1,11 +1,10 @@
-const Discord = require('discord.js'); 
-const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"]});
-const fs = require('fs');
 const PREFIX = "-";
+const fs = require('fs');
+const client = new Discord.Client({ partials: ["MESSAGE", "CHANNEL", "REACTION"]});
 
 const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
 for(const file of commandFiles){
-    const command = require(`../commands/${file}`);
+    const command = require(`./commands/${file}`);
     
     client.commands.set(command.name, command);
 }
