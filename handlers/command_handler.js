f5f6c492a9f8da11c5439892d7481f83b76a9676
MaxWasTaked <61461142+Yonaga678@users.noreply.github.com>
2021-03-19 10:51:03 +0200

    Update command_handler.js

--- handlers/command_handler.js
+++ handlers/command_handler.js
@@ -1,9 +1,9 @@
 const PREFIX = "-";
 const fs = require('fs');
 
-const commandFiles = fs.readdirSync('./commands/').filter(file => file.endsWith('.js'));
+const commandFiles = fs.readdirSync('../commands/').filter(file => file.endsWith('.js'));
 for(const file of commandFiles){
-    const command = require(`./commands/${file}`);
+    const command = require(`../commands/${file}`);
     
     bot.commands.set(command.name, command);
 }
