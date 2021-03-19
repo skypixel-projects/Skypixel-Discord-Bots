8d3e36e93a45be7a30b404f377bb602247e4a3b6
MaxWasTaked <61461142+Yonaga678@users.noreply.github.com>
2021-03-19 21:36:31 +0200

    Update index.js

--- index.js
+++ index.js
@@ -10,5 +10,16 @@ client.events = new Discord.Collection();
     require(`./handlers/${handler}`)(client, Discord);
 })
 
+bot.on('readdy', () => {
+    console.log('setPresence working!');
+    client.user.setPresence({
+        activity: {
+            name: 'maintenance',
+            type: 'WATCHING'
+        },
+        status: 'idle'
+    })
+});
+
 client.login('ODE5Njc4MjM4NjIzNzkzMjEz.YEqG2A.21-58MfTq6SL6iHRahy532RTOeg');
 // client.login(process.env.token);
