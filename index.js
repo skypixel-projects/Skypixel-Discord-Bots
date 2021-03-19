7fd0529765144b8f53e8b5a304406776dbcf231b
MaxWasTaked <61461142+Yonaga678@users.noreply.github.com>
2021-03-19 11:20:04 +0200

    19.03.2021 11:20

--- index.js
+++ index.js
@@ -8,6 +8,6 @@ client.events = new Discord.Collection();
 
 ['command_handler', 'event_handler'].forEach(handler => {
     require(`./handlers/${handler}`)(client, Discord);
-})
+});
 
 client.login(process.env.token);
