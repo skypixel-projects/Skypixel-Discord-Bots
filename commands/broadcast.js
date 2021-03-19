ab05a61abd81a3b6481afadfbbd92c1ec7ef9fcc
MaxWasTaked <61461142+Yonaga678@users.noreply.github.com>
2021-03-19 09:40:33 +0200

    19.03.2021 09:40

--- commands/broadcast.js
+++ commands/broadcast.js
@@ -1,11 +1,10 @@
 module.exports = {
     name: "broadcast",
     description: "",
-    execute(message, args){
+    execute(message){
         if(message.guild.id == '672018546125045760') {
             if(message.author.bot) return;
-            //message.channel.send('```' + args + '```');
-            message.author.send(args);
+            message.author.send(message);
         }
     }
 }
--- commands/development.js
+++ commands/development.js
@@ -6,15 +6,17 @@ module.exports = {
     name: "development",
     description: "",
     execute(message, args){
-        if(message.content.includes('lang_en')){
-            const path = "./language/lang_en.yml";
-            const content = fs.readFileSync(path, 'utf-8');
-            message.channel.send(content);
-        }
-        if(message.content.includes('lang_ro')){
-            const path = "./language/lang_ro.yml";
-            const content = fs.readFileSync(path, 'utf-8');
-            message.channel.send(content);
+        if(message.guild.id == '672018546125045760') {
+            if(message.content.includes('lang_en')){
+                const path = "./language/lang_en.yml";
+                const content = fs.readFileSync(path, 'utf-8');
+                message.channel.send(content);
+            }
+            if(message.content.includes('lang_ro')){
+                const path = "./language/lang_ro.yml";
+                const content = fs.readFileSync(path, 'utf-8');
+                message.channel.send(content);
+            }
         }
     }
 }
