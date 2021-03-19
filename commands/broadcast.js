2a65f3c3721adaa64dcc46501efe90d5a7fb8434
MaxWasTaked <61461142+Yonaga678@users.noreply.github.com>
2021-03-19 09:48:13 +0200

    19.03.2021 09:48

--- commands/broadcast.js
+++ commands/broadcast.js
@@ -1,10 +1,10 @@
 module.exports = {
     name: "broadcast",
     description: "",
-    execute(message){
+    execute(message, args){
         if(message.guild.id == '672018546125045760') {
             if(message.author.bot) return;
-            message.author.send(message);
+            message.author.send(args);
         }
     }
 }
