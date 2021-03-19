d6905e066afd9630753ba88ab07b506db9e3743a
MaxWasTaked <61461142+Yonaga678@users.noreply.github.com>
2021-03-19 10:11:27 +0200

    Update broadcast.js

--- commands/broadcast.js
+++ commands/broadcast.js
@@ -6,10 +6,11 @@ module.exports = {
     execute(message, args){
         if(message.guild.id == '672018546125045760') {
             if(message.author.bot) return;
-            message.author.send(args);
-            
-            //Debug line!
-            message.channel.send(`[DEBUG] (${message}) / (${args})`);
+            if(message.content.startsWith('-kick')) {
+                message.author.send(args);
+                //Debug line!
+                message.channel.send(`[DEBUG] (${message}) / (${args})`);
+            }
         }
     }
 }
