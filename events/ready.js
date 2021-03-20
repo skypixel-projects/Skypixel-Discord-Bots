d091fd323cd270deafb9ec7d2debef0e2c8efd4a
MaxWasTaked <61461142+Yonaga678@users.noreply.github.com>
2021-03-21 01:05:25 +0200

    21.03.2021 01:05

--- events/ready.js
+++ events/ready.js
@@ -6,12 +6,4 @@ module.exports = bot => {
     console.log(lang_en.console_ready)
     bot.user.setActivity(botsettings.discord_profile_activity, {type: 'LISTENING'});
     bot.user.setPresence({status: botsettings.discord_profile_status});
-
-    // bot.user.setPresence({
-    //     status: botsettings.discord_profile_status,  // You can show online, idle... Do not disturb is dnd
-    //     game: {
-    //         name: botsettings.discord_profile_activity,  // The message shown
-    //         type: "LISTENING" // PLAYING, WATCHING, LISTENING, STREAMING,
-    //     }
-    // });
 }
