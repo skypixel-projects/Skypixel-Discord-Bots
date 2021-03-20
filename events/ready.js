c5089560f77ab85de4ba3234b4e991b51fa38f11
MaxWasTaked <61461142+Yonaga678@users.noreply.github.com>
2021-03-21 00:13:18 +0200

    21.03.2021 00:13

--- events/ready.js
+++ events/ready.js
@@ -2,6 +2,6 @@ const Discord = require("discord.js")
 const lang_en = require('../languages/lang_en.json');
 
 module.exports = bot => { 
-    console.log(lang_en.ready)
+    console.log(lang_en.console_ready)
     bot.user.setActivity("MaxWasTaked", {type: 'WATCHING'});
 }
