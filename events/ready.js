f80f0b7ef956a4610baa9f398ed4a26354e6798a
MaxWasTaked <61461142+Yonaga678@users.noreply.github.com>
2021-03-21 00:50:28 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -5,7 +5,7 @@ const lang_en = require('../languages/lang_en.json');
 module.exports = bot => { 
     console.log(lang_en.console_ready)
     bot.user.setActivity(botsettings.discord_profile_activity, {type: 'LISTENING'});
-    bot.user.setPresence({status: discord_profile_status});
+    bot.user.setPresence({status: botsettings.discord_profile_status});
 
     // bot.user.setPresence({
     //     status: botsettings.discord_profile_status,  // You can show online, idle... Do not disturb is dnd
