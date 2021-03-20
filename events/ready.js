c906787dc0e7690cda0370f6a231edf64e1a4b6f
MaxWasTaked <61461142+Yonaga678@users.noreply.github.com>
2021-03-20 23:49:14 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,6 +1,6 @@
 const Discord = require("discord.js")
 
 module.exports = bot => { 
-    console.log(`${bot.user.username} is online`)
+    console.log(`${bot.user.username} is up to running! Please be note this bot is not stable yet!`)
     bot.user.setActivity("MaxWasTaked", {type: 'WATCHING'});
 }
