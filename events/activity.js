2aef00170cb347cc7c795abbf0845e9047a40f38
MaxWasTaked <61461142+Yonaga678@users.noreply.github.com>
2021-03-21 22:11:12 +0200

    Update activity.js

--- events/activity.js
+++ events/activity.js
@@ -4,5 +4,6 @@ const botsettings = require('../botsettings.json');
 const lang_en = require('../languages/lang_en.json');
 
 module.exports = bot => { 
+    console.log("Activity is working")
     bot.user.setActivity(botsettings.discord_profile_activity, { type: 'LISTENING' });
 }
