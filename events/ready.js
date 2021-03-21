2d5924b5d049bd652e8376b27dd124196614ecae
MaxWasTaked <61461142+Yonaga678@users.noreply.github.com>
2021-03-21 08:36:35 +0200

    21.03.2021 08:36

--- events/ready.js
+++ events/ready.js
@@ -1,9 +1,11 @@
 const Discord = require("discord.js")
+
 const botsettings = require('../botsettings.json');
 const lang_en = require('../languages/lang_en.json');
 
 module.exports = bot => { 
     console.log(lang_en.console_ready)
+
     bot.user.setActivity(botsettings.discord_profile_activity, {type: 'LISTENING'});
-    bot.user.setPresence({status: 'idle'});
+    bot.user.setPresence({status: botsettings.discord_profile_status});
 }
