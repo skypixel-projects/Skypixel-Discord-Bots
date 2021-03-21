d6227794cc00368f7a0774b0f02becf2e157e695
MaxWasTaked <61461142+Yonaga678@users.noreply.github.com>
2021-03-21 08:52:55 +0200

    Update eventHandler.js

--- util/eventHandler.js
+++ util/eventHandler.js
@@ -2,4 +2,5 @@ const reqEvent = (event) => require(`../events/${event}`)
 
 module.exports = bot => {
     bot.on("ready", function() {reqEvent("ready") (bot) });
+    bot.om("Presence", function() {reqEvent("Presence" (bot))});
  }
