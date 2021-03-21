1fad870ea3d5fd904a10887e18b2761df2b959bf
MaxWasTaked <61461142+Yonaga678@users.noreply.github.com>
2021-03-21 08:55:37 +0200

    21.03.2021 08:55

--- events/ready.js
+++ events/ready.js
@@ -5,4 +5,12 @@ const lang_en = require('../languages/lang_en.json');
 
 module.exports = bot => { 
     console.log(lang_en.console_ready)
+
+    client.user.setPresence({
+        status: "idle",  // You can show online, idle... Do not disturb is dnd
+        game: {
+            name: "MaxWasTaked",  // The message shown
+            type: "LISTENING" // PLAYING, WATCHING, LISTENING, STREAMING,
+        }
+    });
 }
