2f732929d01079823b2f74b5d520b3cad4e19d22
MaxWasTaked <61461142+Yonaga678@users.noreply.github.com>
2021-03-21 22:43:52 +0200

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -7,9 +7,10 @@ module.exports.run = async (bot, message, args) => {
     let member = message.mentions.members.first() || message.guild.members.cache.get(userArgs[0]) || message.guild.members.cache.find(x => x.user.username.toLowerCase() === userArgs.slice(0).join(" ") || x.user.username === userArgs[0]) || message.member;
 
     if(member.user.bot) return;
+    if(!userArgs) return;
     const img = await member.user.displayAvatarURL();
     const embed = new Discord.MessageEmbed()
-    .setColor('YELLOW')
+    .setColor('RED')
     .setImage(img)
     .setTitle(`${member.user.username} ` + lang_en.commands_avatar_title)
 
