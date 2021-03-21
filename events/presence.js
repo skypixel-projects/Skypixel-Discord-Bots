c84bdd53f526ff8437e10c4335590bfb4ae3ddcf
MaxWasTaked <61461142+Yonaga678@users.noreply.github.com>
2021-03-21 08:50:15 +0200

    Update presence.js

--- events/presence.js
+++ events/presence.js
@@ -4,6 +4,8 @@ const botsettings = require('../botsettings.json');
 const lang_en = require('../languages/lang_en.json');
 
 module.exports = bot => {
+    console.log('Presence event has been loaded!')
+
     bot.user.setActivity(botsettings.discord_profile_activity, {type: 'LISTENING'});
     bot.user.setPresence({status: botsettings.discord_profile_status});
 }
