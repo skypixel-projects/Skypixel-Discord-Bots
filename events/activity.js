980c440bfea2d54ea625f5e4ee31f0ebaa393d0f
MaxWasTaked <61461142+Yonaga678@users.noreply.github.com>
2021-03-21 22:25:53 +0200

    Update activity.js

--- events/activity.js
+++ events/activity.js
@@ -4,6 +4,6 @@ const botsettings = require('../botsettings.json');
 const lang_en = require('../languages/lang_en.json');
 
 module.exports = bot => { 
-    bot.user.setActivity({status: botsettings.discord_profile_status});
+    bot.user.setPresence({status: botsettings.discord_profile_status});
     bot.user.setActivity(botsettings.discord_profile_activity, { type: 'LISTENING' });
 }
