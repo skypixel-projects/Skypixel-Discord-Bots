87d5dec62697bafa2a7e445c20014943d7e3bc48
MaxWasTaked <61461142+Yonaga678@users.noreply.github.com>
2021-03-21 22:30:12 +0200

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -2,11 +2,16 @@ const botsettings = require('../botsettings.json');
 const lang_en = require('../languages/lang_en.json');
 
 module.exports.run = async (bot, message, args) => {
-    if(message.guild.id == '672018546125045760') {
-        if(message.author.bot) return;
-        message.reply(message.author.displayAvatarURL());
-        message.delete();
-    }
+    if(message.author.bot) return;
+    // message.reply(message.author.displayAvatarURL());
+    message.delete();
+
+    const img = await message.author.displayAvatarURL();
+    const embed = new Discord.MessageEmbed()
+    .setImage(img)
+    .setTitle(`${message.author}`)
+
+    message.channel.send(embed);
 }
 
 module.exports.config = {
