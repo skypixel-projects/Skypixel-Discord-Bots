f9d70f0c0dbe680f46aceabb79df17f5c4e29e31
MaxWasTaked <61461142+Yonaga678@users.noreply.github.com>
2021-03-21 21:53:38 +0200

    21.03.2021 21:53

--- events/activity.js
+++ events/activity.js
@@ -4,5 +4,5 @@ const botsettings = require('../botsettings.json');
 const lang_en = require('../languages/lang_en.json');
 
 module.exports = bot => { 
-    console.log(lang_en.console_ready)
+    bot.user.setActivity(botsettings.discord_profile_activity, { type: 'LISTENING' });
 }
