39c126315f73911e5feb556d6de3720437c19dad
MaxWasTaked <61461142+Yonaga678@users.noreply.github.com>
2021-03-21 08:44:35 +0200

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -5,7 +5,12 @@ const lang_en = require('../languages/lang_en.json');
 
 module.exports = bot => { 
     console.log(lang_en.console_ready)
+}
 
-    // bot.user.setPresence({status: botsettings.discord_profile_status});
-    bot.user.setActivity(botsettings.discord_profile_activity, {type: 'LISTENING'}).setPresence({status: botsettings.discord_profile_status});
+module.exports = bot => {
+    bot.user.setActivity(botsettings.discord_profile_activity, {type: 'LISTENING'});
+}
+
+module.exports = bot => {
+    bot.user.setPresence({status: botsettings.discord_profile_status});
 }
