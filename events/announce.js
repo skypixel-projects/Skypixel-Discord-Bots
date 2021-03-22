93e0c93a0c4f002c78fd0bd8629c03d59bcb317b
Yonaga678 <norbertbaricz608@gmail.com>
2021-03-22 23:42:29 +0200

    Update announce.js

--- events/announce.js
+++ events/announce.js
@@ -5,7 +5,7 @@ const lang_en = require('../languages/lang_en.json');
 
 module.exports = bot => { 
     //Aici bot-ul va afla pe care server sa trimita mesajul!
-    const DevGuild = guild.get("672018546125045760");
+    const DevGuild = guilds.get("672018546125045760");
 
     //Bot-ul va trimite un mesaj pe ori ce canal denumit "Welcome"!
     const welcomeChannel = DevGuild.channels.cache.find(channel => channel.name === 'welcome')
