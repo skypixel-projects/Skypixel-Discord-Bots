50b89addb755285463db5bf981e8ae867253a2d4
Yonaga678 <norbertbaricz608@gmail.com>
2021-03-22 23:33:03 +0200

    Update announce.js

--- events/announce.js
+++ events/announce.js
@@ -4,8 +4,8 @@ const botsettings = require('../botsettings.json');
 const lang_en = require('../languages/lang_en.json');
 
 module.exports = bot => { 
-    const guildChannel = guild.get("672018546125045760");
+    const guild = client.channels.get("672018546125045760");
 
-    const welcomeChannel = guildChannel.channels.cache.find(channel => channel.name === 'welcome')
+    const welcomeChannel = guild.channels.cache.find(channel => channel.name === 'welcome')
     welcomeChannel.send (`Welcome! ${member}`)
 }
