6fbbe844d3d16bf83252e7141ddfbb64fb3f56d7
Yonaga678 <norbertbaricz608@gmail.com>
2021-03-23 00:38:05 +0200

    Update index.js

--- index.js
+++ index.js
@@ -9,30 +9,30 @@ const bot = new Discord.Client({disableEveryone: true});
 // })
 
 require("./util/eventHandler")(bot)
-require("./util/commandHandler")(bot)
+// require("./util/commandHandler")(bot)
 
 
 const fs = require("fs");
 bot.commands = new Discord.Collection();
 bot.aliases = new Discord.Collection();
 
-// fs.readdir("./commands/", (err, files) => {
+fs.readdir("./commands/", (err, files) => {
 
-//     if(err) console.log(err)
+    if(err) console.log(err)
 
-//     let jsfile = files.filter(f => f.split(".").pop() === "js") 
-//     if(jsfile.length <= 0) {
-//         return console.log("[LOGS] Couldn't Find Commands!");
-//     }
+    let jsfile = files.filter(f => f.split(".").pop() === "js") 
+    if(jsfile.length <= 0) {
+        return console.log("[LOGS] Couldn't Find Commands!");
+    }
 
-//     jsfile.forEach((f, i) => {
-//         let pull = require(`./commands/${f}`);
-//         bot.commands.set(pull.config.name, pull);  
-//         pull.config.aliases.forEach(alias => {
-//             bot.aliases.set(alias, pull.config.name)
-//         });
-//     });
-// });
+    jsfile.forEach((f, i) => {
+        let pull = require(`./commands/${f}`);
+        bot.commands.set(pull.config.name, pull);  
+        pull.config.aliases.forEach(alias => {
+            bot.aliases.set(alias, pull.config.name)
+        });
+    });
+});
 
 bot.on("message", async message => {
     if(message.author.bot || message.channel.type === "dm") return;
