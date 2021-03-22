0f2d9f64031a12094e99d830a7686abfedfe5b3a
Yonaga678 <norbertbaricz608@gmail.com>
2021-03-23 00:27:17 +0200

    23.03.2021 12:27PM

--- events/activity.js
+++ events/activity.js
@@ -4,6 +4,6 @@ const botsettings = require('../botsettings.json');
 const lang_en = require('../languages/lang_en.json');
 
 module.exports = bot => { 
-    bot.user.setPresence({status: botsettings.discord_profile_status});
+    bot.user.setPresence({status: 'idle'});
     bot.user.setActivity(botsettings.discord_profile_activity, { type: 'LISTENING' });
 }
