882e70ef5dbe8e46f1ce45c41c4a7b742ab7c801
Yonaga678 <norbertbaricz608@gmail.com>
2021-03-22 23:36:09 +0200

    Update announce.js

--- events/announce.js
+++ events/announce.js
@@ -6,6 +6,11 @@ const lang_en = require('../languages/lang_en.json');
 module.exports = bot => { 
     const guild = client.channels.get("672018546125045760");
 
+    //Bot-ul va trimite un mesaj pe ori ce canal denumit "Welcome"!
     const welcomeChannel = guild.channels.cache.find(channel => channel.name === 'welcome')
     welcomeChannel.send (`Welcome! ${member}`)
+
+    //Bot-ul va adauga automat rolul de default!
+    let DefaultRole = message.guild.roles.cache.get('677939383038640199');
+    member.roles.add(DefaultRole);
 }
