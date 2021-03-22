a82dcd7690c6815462a83558dc8da959df59d10d
Yonaga678 <norbertbaricz608@gmail.com>
2021-03-23 00:33:38 +0200

    22.03.2021 12:33PM

---
+++ util/commandHandler.js
@@ -0,0 +1,23 @@
+const Discord = require('discord.js');
+
+const bot = new Discord.Client({disableEveryone: true});
+
+module.exports = bot => {
+    fs.readdir("./commands/", (err, files) => {
+
+        if(err) console.log(err)
+    
+        let jsfile = files.filter(f => f.split(".").pop() === "js") 
+        if(jsfile.length <= 0) {
+            return console.log("[LOGS] Couldn't Find Commands!");
+        }
+    
+        jsfile.forEach((f, i) => {
+            let pull = require(`./commands/${f}`);
+            bot.commands.set(pull.config.name, pull);  
+            pull.config.aliases.forEach(alias => {
+                bot.aliases.set(alias, pull.config.name)
+            });
+        });
+    });
+}
--- util/eventHandler.js
+++ util/eventHandler.js
@@ -9,4 +9,4 @@ module.exports = bot => {
     bot.on("ready", function() {reqEvent("ready") (bot) });
     bot.on("ready", function() {reqEvent("activity") (bot) });
     bot.on("guildMemberAdd", function() {reqEvent("announce") (bot) });
- }
+}
