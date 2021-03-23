7b84308520ca66ddfe926a5b166bc2237324e310
Yonaga678 <norbertbaricz608@gmail.com>
2021-03-24 00:54:44 +0200

    24.03.2021 12:54AM

--- commands/nsfw.js
+++ commands/nsfw.js
@@ -1,10 +1,17 @@
 const Discord = require('discord.js');
+const fs = require("fs")
+
+const botsettings = require('../botsettings.json');
+const lang_en = require('../languages/lang_en.json');
 
 module.exports.run = async (bot, message, args) => {
     message.delete();
 
-    message.channel.send(`The command is working!`);
-
+    var embed = new Discord.MessageEmbed()
+        .setAuthor(lang_en.maintenance)
+        .setColor('RED')
+            
+    message.channel.send(embed);
 }
 
 module.exports.config = {
