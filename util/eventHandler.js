63bb22a1273bee78d1b352c98bff8947f4aa40bd
MaxWasTaked <61461142+Yonaga678@users.noreply.github.com>
2021-03-27 20:07:25 +0300

    Update eventHandler.js

--- util/eventHandler.js
+++ util/eventHandler.js
@@ -6,15 +6,7 @@ module.exports = bot => {
     // bot.on("ready"<-- Event name here!
 
 
-<<<<<<< HEAD
     bot.on("ready", function() {reqEvent("ready") (bot) })
     bot.on("ready", function() {reqEvent("activity") (bot) })
     bot.on("messageReactionAdd", function() {reqEvent("react") (bot) })
- }
-=======
-    bot.on("ready", function() {reqEvent("ready") (bot) });
-    bot.on("ready", function() {reqEvent("activity") (bot) });
-    bot.on("ready", function() {reqEvent("dakota") (bot) });
-    bot.on("guildMemberAdd", function() {reqEvent("announce") (bot) });
 }
->>>>>>> d3c7147546fbff93bcf0f1d3e5dd64eb7b12bb66
