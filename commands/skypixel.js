991d279a7d2cc5a258afebbe4a9fbbbda75dd852
MaxWasTaked <61461142+Yonaga678@users.noreply.github.com>
2021-04-06 00:46:47 +0300

    06.04.2021 00:46
    New command "-web, -site, -website" has been added!

---
+++ commands/skypixel.js
@@ -0,0 +1,17 @@
+const Discord = require('discord.js');
+const lang_en = require('../languages/lang_en.json');
+
+const hook = new Discord.WebhookClient('828735435332517990', 'QQh6rSiyRsejSU5rpdH9y9LaSoa14aHo2Zhlt1cVO4wDfwPiy37bSlLVX3Qma1aC1q2S');
+
+module.exports.run = async (bot, message, args) => {
+    message.delete();
+    hook.send(lang_en.skypixel_official_website_link);
+}
+
+module.exports.config = {
+    name: "website",
+    description: "",
+    usage: "",
+    accessableby: "Members",
+    aliases: ['web', 'site']
+}
