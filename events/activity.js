11d437db7dd487f3d91d47bda97c54e75a0260f3
MaxWasTaked <61461142+Yonaga678@users.noreply.github.com>
2021-04-23 22:54:40 +0300

    23.04.2021 22:54

--- events/activity.js
+++ events/activity.js
@@ -4,6 +4,7 @@ const botsettings = require('../botsettings.json');
 const lang_en = require('../languages/lang_en.json');
 
 module.exports = bot => { 
-    bot.user.setPresence({status: `${botsettings.discord_profile_status}`});
+    // bot.user.setPresence({status: `${botsettings.discord_profile_status}`});
+    bot.user.setStatus("dnd");
     bot.user.setActivity(botsettings.discord_profile_activity, { type: botsettings.discord_profile_activity_type });
 }
