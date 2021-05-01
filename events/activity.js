6569f67f17a92b3ab2b9635f256244cd0ad71c95
Soba <80915881+xSoberChris@users.noreply.github.com>
2021-05-01 19:42:45 +0300

    01.04.2021 7:42PM

--- events/activity.js
+++ events/activity.js
@@ -5,5 +5,12 @@ const lang_en = require('../languages/lang_en.json');
 
 module.exports = bot => { 
     bot.user.setPresence({status: `${botsettings.discord_profile_status}`});
-    bot.user.setActivity(botsettings.discord_profile_activity, { type: botsettings.discord_profile_activity_type });
+    // bot.user.setActivity(botsettings.discord_profile_activity, { type: botsettings.discord_profile_activity_type });
+
+   
+    setInterval(function(){ 
+        // bot.channels.cache.get("835600980892319834").send("<@762368975094022155> <@351664932271685632> <@726678975501303809> <@819678238623793213> <@764884863668846592> <@236811109066211329>");
+        //  console.log('Alive packet')
+        bot.user.setActivity(botsettings.discord_profile_activity, { type: botsettings.discord_profile_activity_type });
+        }, 60000);
 }
