0b46607efcf46a13b025535e8477fc03d6583a0c
MaxWasTaked <61461142+Yonaga678@users.noreply.github.com>
2021-05-02 19:26:47 +0300

    01.05.2021 19:26

--- events/announce.js
+++
@@ -1,8 +0,0 @@
-const Discord = require("discord.js")
-
-const botsettings = require('../botsettings.json');
-const lang_en = require('../languages/lang_en.json');
-
-module.exports = bot => { 
-    console.log(`Debug -> A new member has joined to an guild!`)
-}
