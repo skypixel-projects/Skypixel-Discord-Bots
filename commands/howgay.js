b40f7b60a20e5e110d95ed8592ce3c2fe4abcfb8
MaxWasTaked <61461142+Yonaga678@users.noreply.github.com>
2021-05-09 00:10:52 +0300

    09.05.2021 00:10

---
+++ commands/howgay.js
@@ -0,0 +1,29 @@
+const Discord = require('discord.js');
+const bot = new Discord.Client();
+
+const botsettings = require('../botsettings.json');
+const lang_en = require(`../languages/${botsettings.default_lang_for_discord_bot}.json`);
+
+const fs = require('fs');
+
+module.exports.run = async (bot, message, args) => {
+    message.delete();
+
+    let member = message.mentions.users.first() || message.author
+
+    let rng = Math.floor(Math.random() * 101);
+
+    let embed = new Discord.MessageEmbed()
+        .setTitle(`${member.username} is ` + rng + `% Gay 🤣`)
+        .setColor(botsettings.embed_color_message_discord_bot)
+        .setFooter('Asked by ' + message.author.username, message.author.displayAvatarURL())
+    message.channel.send(embed);
+}
+
+module.exports.config = {
+    name: "howgay",
+    description: "",
+    usage: "",
+    accessableby: "Members",
+    aliases: []
+}
