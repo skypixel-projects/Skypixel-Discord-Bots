50bbad04b61c214ee313f1666dc0d0f65e689cde
MaxWasTaked <61461142+Yonaga678@users.noreply.github.com>
2021-06-12 00:28:03 +0300

    12.06.2021 12:27

--- commands/help.js
+++ commands/help.js
@@ -5,58 +5,16 @@ const lang_en = require(`../languages/${botsettings.default_lang_for_discord_bot
 
 
 module.exports.run = async (bot, message, args) => {
-    message.delete();
-
     let helpArray = message.content.split(" ");
     let helpArgs = helpArray.slice(1);
 
-    // The main command handler
-    if(!helpArgs[0]) {
-        var embed = new Discord.MessageEmbed()
-            .setAuthor(lang_en.commands_help_author)
-            .setDescription(lang_en.commands_help_description)
-            .addFields({ name: lang_en.commands_help_prefix, value: '```' + `${botsettings.prefix}` + '```', inline: true})
-            .addFields({ name: 'More help', value: '```' + `-help <commands>` + '```', inline: true})
-            .setColor(botsettings.embed_color_message_discord_bot)
-            .setFooter('Asked by ' + message.author.username, message.author.displayAvatarURL())
-        message.channel.send(embed);
-    }
-
-    // The music command handler
-    if(args === "music"){
-        var embed = new Discord.MessageEmbed()
-            .addFields({ name: "Music commands", value: '```' + lang_en.commands_help_description_1 + '```', inline: true})
-            .setColor(botsettings.embed_color_message_discord_bot)
-            .setFooter('Asked by ' + message.author.username, message.author.displayAvatarURL())
-        message.channel.send(embed);
-    }
-
-    // The fun command handler
-    if(args === "fun"){
-        var embed = new Discord.MessageEmbed()
-            .addFields({ name: "Fun commands", value: '```' + lang_en.commands_help_description_2 + '```', inline: true})
-            .setColor(botsettings.embed_color_message_discord_bot)
-            .setFooter('Asked by ' + message.author.username, message.author.displayAvatarURL())
-        message.channel.send(embed);
-    }
-
-    // The admin command handler
-    if(args === "admin"){
-        var embed = new Discord.MessageEmbed()
-            .addFields({ name: "Admin commands", value: '```' + lang_en.commands_help_description_3 + '```', inline: true})
-            .setColor(botsettings.embed_color_message_discord_bot)
-            .setFooter('Asked by ' + message.author.username, message.author.displayAvatarURL())
-        message.channel.send(embed);
-    }
-
-    // The random command handler
-    if(args === "random"){
-        var embed = new Discord.MessageEmbed()
-            .addFields({ name: "Random commands", value: '```' + lang_en.commands_help_description_4 + '```', inline: true})
-            .setColor(botsettings.embed_color_message_discord_bot)
-            .setFooter('Asked by ' + message.author.username, message.author.displayAvatarURL())
-        message.channel.send(embed);
-    }
+    var embed = new Discord.MessageEmbed()
+        .setTitle(`${lang_en.commands_help_title}`)
+        .setDescription(`${lang_en.commands_help_description} \n \n https://yonaga678.github.io/SK-DISCORDBOTS/`)
+        .setColor(botsettings.embed_color_message_discord_bot)
+        .setThumbnail(bot.user.displayAvatarURL())
+        .setFooter('Asked by ' + message.author.username, message.author.displayAvatarURL())
+    message.lineReply(embed);
 }
 
 module.exports.config = {
