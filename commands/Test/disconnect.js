e19d6efb04ebdbc81b9ea55fbab94b1844e93a26
Skypixel <61461142+skypixel-projects@users.noreply.github.com>
2021-07-16 19:49:52 +0300

    16.07.2021 19:49

--- commands/Test/disconnect.js
+++ commands/Test/disconnect.js
@@ -5,12 +5,25 @@ const lang_en = require(`../../languages/${botsettings.default_lang_for_discord_
 
 
 module.exports.run = async (bot, message, args) => {
-    const memberTarget = message.mentions.users.first();
-
-    memberTarget.voice
-        .kick('Kicked!')
-        .then((users) => message.lineReply(`${users} has been kicked from the voice channel!`))
-        .catch(console.error);
+    const user = message.mentions.users.first();
+    if (user) {
+        const member = message.guild.member(user);
+        if (member) {
+          member.voice
+            .kick('Optional reason that will display in the audit logs')
+            .then(() => {
+                message.lineReply(`Successfully kicked from the voice ${user.tag}!`);
+            })
+            .catch(err => {
+                message.lineReply('I was unable to kick the member :(');
+                console.error(err);
+            });
+        } else {
+            message.lineReply("That user isn't in this guild!");
+        }
+    } else {
+        message.lineReply("You didn't mention the user to kick!");
+    }
 }
 
 module.exports.config = {
@@ -18,5 +31,5 @@ module.exports.config = {
     description: "",
     usage: "",
     accessableby: "Admin",
-    aliases: ["disconnect", "disc"]
+    aliases: ["disconnect", "vckick", "disc"]
 }
