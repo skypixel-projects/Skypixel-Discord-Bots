e292e780f7efc7cb897b9c213e8c5415e6717837
Skypixel <61461142+skypixel-projects@users.noreply.github.com>
2021-07-20 12:20:34 +0300

    Dakota AC has been removed. Issues fixed. Added premium mode!

---
+++ events/Servers/MineXx.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js")
+const botsettings = require('../../botsettings.json');
+const lang_en = require('../../languages/lang_en.json');
+const { bot } = require('../../index');
+
+// Check for the "WTF" message and replay with some message.
+bot.on('message', message => {
+    var server = message.guild.id;
+
+    if (server === "786144853754904587") {
+        if (message.content.includes("wtf")) {
+            message.lineReply("WTF = Welcome To Facebook");
+        }
+    }
+})
--- util/eventHandler.js
+++ util/eventHandler.js
@@ -2,10 +2,32 @@ const Discord = require("discord.js");
 const fs = require("fs");
 
 module.exports = bot => {
-    const eventFiles = fs.readdirSync('././events').filter(file => file.endsWith('.js'));
+    const eventFiles_events = fs.readdirSync('././events').filter(file => file.endsWith('.js'));
 
-    for (const file of eventFiles) {
+    for (const file of eventFiles_events) {
         const event = require(`../events/${file}`);
+        if (event.once) {
+            bot.once(event.name, (...args) => event.execute(...args, Discord, bot));
+        } else {
+            bot.on(event.name, (...args) => event.execute(...args, Discord, bot));
+        }
+    }
+
+    const eventFiles_servers = fs.readdirSync('././events/Servers/').filter(file => file.endsWith('.js'));
+
+    for (const file of eventFiles_servers) {
+        const event = require(`../events/Servers/${file}`);
+        if (event.once) {
+            bot.once(event.name, (...args) => event.execute(...args, Discord, bot));
+        } else {
+            bot.on(event.name, (...args) => event.execute(...args, Discord, bot));
+        }
+    }
+
+    const eventFiles_security = fs.readdirSync('././events/Security/').filter(file => file.endsWith('.js'));
+
+    for (const file of eventFiles_security) {
+        const event = require(`../events/Security/${file}`);
         if (event.once) {
             bot.once(event.name, (...args) => event.execute(...args, Discord, bot));
         } else {
